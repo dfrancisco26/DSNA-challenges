470f2f7f2cb7d9560a440f004b83c69484275b05
David Francisco <dfrancisco2677@gmail.com>
2022-09-19 16:55:38 -0700

    at function and test written.

--- day-01/index.js
+++ day-01/index.js
@@ -13,7 +13,17 @@ function titleCaseWords(str) {
     .join(' ');
 }
 
+function oddishEvenish(number) {
+  let num = 0;
+  number
+    .toString()
+    .split('')
+    .map(number => num += Number(number));
+  return num % 2 === 0 ? 'evenish' : 'oddish';
+}
 
+function at(arr, i) {
+  return i >= 0 ? arr[i] : arr[arr.length + i];
+}
 
-
-module.exports = { reverseSentence, titleCaseWords };
+module.exports = { reverseSentence, titleCaseWords, oddishEvenish, at };
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,14 +1,18 @@
-const { reverseSentence,  titleCaseWords } = require('./index');
+const { reverseSentence,  titleCaseWords, oddishEvenish, at } = require('./index');
 
 describe('primitive code challenges', () => {
-  describe('reverseSentence', () => {
-    it('should reverse a sentence', () => {
-      expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
-    });
+  it('should reverse a sentence', () => {
+    expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
   });
-  describe('capFirstLetter', () => {
-    it('should capitalize first letter of every word', () => {
-      expect(titleCaseWords('dude, whEre\'s my CAR?')).toEqual('Dude, Where\'s My Car?');
-    });
+  it('should capitalize first letter of every word', () => {
+    expect(titleCaseWords('dude, whEre\'s my CAR?')).toEqual('Dude, Where\'s My Car?');
+  });
+  it('should determine if a number is oddish or evenish', () => {
+    expect(oddishEvenish(121)).toBe('evenish');
+    expect(oddishEvenish(41)).toBe('oddish');
+  });
+  it('should take an array and index and return item located there', () => { 
+    expect(at(['a', 'bravo', 'charlie', 'delta', 'e'], 2)).toEqual('charlie');
+    expect(at(['a', 'bravo', 'charlie', 'delta', 'e'], -2)).toEqual('delta');
   });
 });
